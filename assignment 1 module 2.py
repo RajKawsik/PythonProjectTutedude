036404a1440a28096bca058e2d168642becb7cf4
RajKawsik <rajkawsik@gmail.com>
2025-08-21 14:23:53 +0530

    Assignment 1 module

---
+++ assignment 1 module 2.py
@@ -0,0 +1,21 @@
+#Task 1: Basic mathematical operations
+
+a=int(input('a:'))
+b=int(input('b:'))
+
+addition = a+b
+subtraction = a-b
+multiplication = a*b
+division = a/b
+
+print('addition =',addition)
+print('subtraction =',subtraction)
+print('multiplication =',multiplication)
+print('division =',division)
+
+#Task 2: Personalized greeting
+
+a= input('enter your first name:')
+b= input('enter your last name:')
+
+print("Hello"+","+" ", a + b +"!"+ " Welcome to the Python program.")
